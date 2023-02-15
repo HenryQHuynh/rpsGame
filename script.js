8e179bcb750adefbaabe11f06e96ea4421ab6cb4
Henry Huynh <henryqhuynh@gmail.com>
2023-02-14 23:15:20 -0800

    Adding in functionality for user selection and winning criteria

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 const selectionButtons = document.querySelectorAll('[data-selection]')
+const SELECTIONS = [
+   {
+      name: 'fire',
+      pic: './pics/fire.png',
+      beats: 'grass'
+   },
+   {
+      name: 'water',
+      pic: './pics/water.png',
+      beats: 'fire'
+   },
+   {
+      name: 'grass',
+      pic: './pics/grass.png',
+      beats: 'water'
+   }
+]
 
 selectionButtons.forEach(selectionButton => {
-   selectionButton.addEventlistener('click', e => {
-      const selectionName = selectionButton.dataset.selection
-      makeSelection(selectionName)
+   selectionButton.addEventListener('click', e => {
+     const selectionName = selectionButton.dataset.selection
+     const selection = SELECTIONS.find(selection => selection.name === selectionName)
+     makeSelection(selection);
    })
-})
+ })
 
 function makeSelection(selection) {
-   console.log(selection)
+   const computerSelection = randomSelection()
+   const youreWinner = isWinner(selection, computerSelection)
+   const computerWinner = isWinner(computerSelection, selection)
+   console.log(computerSelection)
 }
+
+function isWinner(selection, opponentSelection) {
+   return selection.beats === opponentSelection.name
+   
+}
+
+function randomSelection() {
+   const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+   return SELECTIONS[randomIndex]
+}
+
